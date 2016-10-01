a21fd6dd3c7c05a3a343a62932e015a5cdf88256
schmjdt <gitschmjdt@gmail.com>
2016-09-30 20:46:48 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('default',['jshint','test','serve']);
+
+gulp.task('jshint',()=>{
+	return gulp.src('./*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+})
+.task('test',()=>{
+	require('./test.js');
+})
+.task('serve',()=>{
+	require('./main.js');
+});
